15a4470b43fb446ca8b743efa6af5f32d1e078cc
Joseph Dewar <superjoeyd123@gmail.com>
2023-06-05 20:36:58 -0700

    working? and untested interp

--- asgn8.rkt
+++ asgn8.rkt
@@ -4,12 +4,15 @@
 (require typed/rackunit)
 
 ;;Value types
-(define-type ValV (U CloV PrimV StrV NumV BoolV))
+(define-type ValV (U CloV PrimV StrV NumV BoolV VoidV ArrV))
 (struct CloV ([params : (Listof Symbol)] [body : ExprC] [env : Env])#:transparent)
 (struct PrimV ([name : Symbol] [arity : Natural])#:transparent)
 (struct StrV ([val : String])#:transparent)
 (struct NumV ([val : Real])#:transparent)
 (struct BoolV ([val : Boolean])#:transparent)
+(struct VoidV ()#:transparent)
+(struct ArrV ([val : (Mutable-Vectorof NumV)])#:transparent)
+
 
 ; Define the type environment data structure
 (define-type TypeEnv (Listof TypeBind))
@@ -44,7 +47,7 @@
 
 ;; Define the environment data type
 (define-type Env (Listof bind))
-(struct bind[(name : Symbol) (val : ValV)] #:transparent)
+(struct bind[(name : Symbol) (val : (Boxof ValV))] #:transparent)
 (define mt-env empty)
 
 
@@ -91,7 +94,7 @@
     [else #t]))
 
 ;; Define the lookup function for environments
-(define (lookup [for : Symbol] [env : Env]) : ValV
+(define (lookup [for : Symbol] [env : Env]) : (Boxof ValV)
   (match env
     [(list) (error 'lookup "VVQS: name not found in environment")]
     [(cons (bind name val) rest-env)
@@ -112,15 +115,15 @@
 (define (top-interp [prog-sexp : Sexp])
   ;; Define the top-level environment
   (define top-env
-    (list (bind '+ (PrimV '+ 2))
-          (bind '- (PrimV '- 2))
-          (bind '* (PrimV '* 2))
-          (bind '/ (PrimV '/ 2))
-          (bind '<= (PrimV '<= 2))
-          (bind 'equal? (PrimV 'equal? 2))
-          (bind 'true (BoolV #t))
-          (bind 'false (BoolV #f))
-          (bind 'error (PrimV 'error 1))))
+    (list (bind '+ (box (PrimV '+ 2)))
+          (bind '- (box (PrimV '- 2)))
+          (bind '* (box (PrimV '* 2)))
+          (bind '/ (box (PrimV '/ 2)))
+          (bind '<= (box (PrimV '<= 2)))
+          (bind 'equal? (box (PrimV 'equal? 2)))
+          (bind 'true (box (BoolV #t)))
+          (bind 'false (box (BoolV #f)))
+          (bind 'error (box (PrimV 'error 1)))))
   (define top-tenv
     (list (TypeBind '+ (FunT (list (NumT) (NumT)) (NumT)))
           (TypeBind '- (FunT (list (NumT) (NumT)) (NumT)))
@@ -151,8 +154,8 @@
     [(? symbol? (? ValidSymbol? s)) (IdC s)]
     [(? string? s) (StrC s)]
     [(list (? symbol? (? ValidSymbol? id)) '<- val) (SetC id (parse val))]
-    [(list body 'if test 'then then)
-     (IfC (parse body) (parse test) (parse then))]
+    [(list body 'if test 'else else)
+     (IfC (parse body) (parse test) (parse else))]
     [(list body 'where (list (list (list (? symbol? (? ValidSymbol? bindings)) ': ty) ':= exp) ...))
      (if (= (length bindings) (length (remove-duplicates bindings)))
            (AppC (LamC (cast bindings (Listof Symbol)) (parse-types (cast ty (Listof Sexp))) (parse body))
@@ -235,35 +238,55 @@
             #f)])]))
 
 
-;;;updated interp function to handle VVQS5, supports enviorments
-;(define (interp [expr : ExprC] [env : Env]) : ValV
-;  (match expr
-;    [(NumC n) (NumV n)]
-;    [(StrC s) (StrV s)]
-;    [(IfC do? test else?)
-;     (define test-result (interp test env))
-;     (match test-result
-;       [(BoolV #t) (interp do? env)]
-;       [(BoolV #f) (interp else? env)]
-;       [else (error 'interp "VVQS: Test expression in if must return a boolean")])]
-;    [(LamC args body) (CloV args body env)]
-;    [(IdC id)
-;     (lookup id env)]
-;    [(AppC fun args)
-;     (define func-val (interp fun env))
-;     (define arg-values (map (λ (arg) (interp (cast arg ExprC) env)) args))
-;     (match func-val
-;       [(CloV params body closure-env)
-;        (if (= (length params) (length arg-values))
-;            (let ([extended-env (append (map (λ (param arg) (bind (cast param Symbol)
-;                                                                  (cast arg ValV))) params arg-values) closure-env)])
-;              (interp body extended-env))
-;            (error 'interp (format "VVQS: Wrong number of arguments in application")))]
-;       [(PrimV name arity)
-;        (if (= arity (length arg-values))
-;            (apply-prim func-val arg-values env)
-;            (error 'interp (format "VVQS: Wrong number of arguments for primitive ~a" name)))]
-;       [else (error 'interp "VVQS: Attempted to apply non-function value")])]))
+;;updated interp function to handle VVQS5, supports enviorments
+(define (interp [expr : ExprC] [env : Env]) : ValV
+  (match expr
+    [(NumC n) (NumV n)]
+    [(StrC s) (StrV s)]
+    [(IfC do? test else?)
+     (define test-result (interp test env))
+     (match test-result
+       [(BoolV #t) (interp do? env)]
+       [(BoolV #f) (interp else? env)]
+       [else (error 'interp "VVQS: Test expression in if must return a boolean")])]
+    [(LamC args arg-types body) (CloV args body env)]
+    [(IdC id)
+     (let [(bindval (lookup id env))]
+       (match bindval
+         [(box val) val]))]
+    [(AppC fun args)
+     (define func-val (interp fun env))
+     (define arg-values (map (λ (arg) (interp (cast arg ExprC) env)) args))
+     (match func-val
+       [(CloV params body closure-env)
+        (if (= (length params) (length arg-values))
+            (let ([extended-env (append (map (λ ([param : Symbol] [arg : ValV]) (bind param (box arg))) params arg-values) closure-env)])
+              (interp body extended-env))
+            (error 'interp (format "VVQS: Wrong number of arguments in application")))]
+       [(PrimV name arity)
+        (if (= arity (length arg-values))
+            (apply-prim func-val arg-values env)
+            (error 'interp (format "VVQS: Wrong number of arguments for primitive ~a" name)))]
+       [else (error 'interp "VVQS: Attempted to apply non-function value")])]
+     [(SetC id val)
+     (let [(bindval (lookup id env))]
+       (match bindval
+         [(box _) (begin (set-box! bindval (interp val env)) (VoidV))]) ; set-box!
+       )]
+    [(BegC exps) (match exps
+                   ['() (VoidV)] ; if the list is empty, return void
+                   [(cons _ _) (let [(values (map (λ ([e : ExprC]) (interp e env)) exps))]
+                                 (if (empty? values) 
+                                     (VoidV)
+                                     (let [(last-value (car (reverse values)))] ; get the last value
+                                       (match last-value
+                                         [(VoidV) (NumV 0)] ; if the last value is void, return NumV 0
+                                         [else last-value]))))])]
+    [(ArrC exps) (define values (map (λ ([e : ExprC]) : ValV (interp e env)) exps))
+                 (if (andmap (λ ([v : ValV]) (NumV? v)) values) ; check each value is a NumV
+                     (ArrV (list->vector values))
+                     (error 'interp "VVQS: All elements of an array must be numbers"))] ; return vector with evaluated expressions
+))
 
 ;; Apply a primitive operation based on its name
 (: apply-prim (PrimV (Listof ValV) Env -> ValV))
@@ -330,7 +353,7 @@
 (check-equal? (parse '(x <- 10)) (SetC 'x (NumC 10)))
 
 ;; Test for if-then-else
-(check-equal? (parse '(x if y then z))
+(check-equal? (parse '(x if y else z))
               (IfC (IdC 'x) (IdC 'y) (IdC 'z)))
 
 ;; Test for where clause
@@ -348,14 +371,7 @@
 
 ;; Test for recursive function
 (check-equal? (parse '(letrec (f ((x : num)) : num => x) in f))
-              (RecC
-               'f
-               '(x)
-               (list (NumT))
-               (IdC 'x)
-               (NumT)
-               (IdC 'f))
-)
+              (RecC 'f (list 'x) (list (NumT)) (IdC 'x) (NumT) (IdC 'f)))
 
 ;; Test for begin expression
 (check-equal? (parse '(begin x y z)) (BegC (list (IdC 'x) (IdC 'y) (IdC 'z))))
